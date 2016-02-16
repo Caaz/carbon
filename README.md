770ecfc4d3d6f5d6e0125bcd007b56e239455f67
Daniel Caaz <DCaazy@gmail.com>
2016-02-16 01:43:08 -0800

    Initial working.
    Everything works as expected so far. Probably.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Carbon
-A web application framework utilizing jade as the view engine.
+A quick to use web framework which utilizes jade and scss for front end shenanigans.
+This framework is designed to be as minimal and barebones as possible, yet powerful enough to be able to create dynamic web servers with ease.
+
+## Why did I make this?
+I used to use express, but the lack of secure servers disappointed me. I could have forked it and made that a possibility, but because it was a port of the express js framework, I didn't want to rear my ugly head into that.
+Because of my previous experience with express, this framework uses a similar approach to handling routes and what not. Features are created as I need them.
--- carbon.dart
+++ carbon.dart
@@ -1,19 +1,28 @@
 library carbon;
 import 'dart:io';
 import "package:jaded/jaded.dart" as jade;
-import 'jade.views.dart' deferred as views;
-
+// part 'utils.dart';
 // RegExp fname = new RegExp(r'.+/(.+?)\.(?:.+?)$');
 // RegExp underscore = new RegExp(r'/_.+$');
 
 class Carbon {
-  String publicDir;
-  String sassDir;
-  String jadeDir;
-  String compileDir;
-  Carbon({this.publicDir: 'public', this.sassDir: 'app/sass', this.jadeDir: 'app/jade', this.compileDir: 'public/compile'}) { _compile(); }
-  listen(InternetAddress address, int port, { String chain:null, String key:null, String password:null }) {
-    var _handleServer = (HttpServer server) => server.listen(_handleRequest);
+  static final Map<String,RegExp> _types = {
+    'application': new RegExp(r'\.(json|xml|octet|dart)$'),
+    'text': new RegExp(r'\.(html?|css)$'),
+    'image': new RegExp(r'\.(jpe?g|gif|png)$')
+  };
+  List<Route> _routes = new List();
+  Map<String,Function> _views;
+  String dirPublic;
+  String dirScss;
+  String dirJade;
+  String dirCompile;
+  Carbon({this.dirPublic: 'public', this.dirScss: 'app/scss', this.dirJade: 'app/jade/pages/', this.dirCompile: 'public/compile'}) { _compile(); }
+  listen(InternetAddress address, int port, { String chain:'', String key:'', String password:'' }) {
+    var _handleServer = (HttpServer server) {
+      server.listen(_handleRequest);
+      _log("Listening at "+server.address.host+":"+server.port.toString());
+    };
     if(chain.isNotEmpty || key.isNotEmpty || password.isNotEmpty) {
       SecurityContext security = new SecurityContext();
       if(chain.isNotEmpty) security.useCertificateChain(Platform.script.resolve(chain).toFilePath());
@@ -23,22 +32,65 @@ class Carbon {
       }
       HttpServer.bindSecure(address, port, security).then(_handleServer);
     }
-    else HttpServer.bind(address,port) .then(_handleServer);
+    else HttpServer.bind(address,port).then(_handleServer);
   }
+  render(HttpResponse response, String page, [Map locals = null]) {
+    String jadeKey = dirJade+page+'.jade';
+    // _log("rendering $jadeKey");
+    // _log(_views.keys.join(", "));
+    if(_views.containsKey(jadeKey))
+      response
+      ..statusCode = HttpStatus.OK
+      ..headers.contentType = ContentType.HTML
+      ..write(_views[jadeKey](locals))
+      ..close();
+    else _notFound(response);
+  }
+  route(String method, String at, RouteHandler handler) => _routes.add(new Route(method,at,handler));
+  views(Map<String,Function> jadeViews) => _views = jadeViews;
   _compile() {
     RegExp fileName = new RegExp(r'.+/(.+?)\.(?:.+?)$');
     RegExp underscore = new RegExp(r'/_.+$');
+    // Create directories if they don't exist!
+    for(String dir in [dirPublic,dirScss,dirJade,dirCompile]) new Directory(dir).createSync(recursive: true);
     // Compile stylesheets.
-    for (var file in new Directory(sassDir).listSync())
+    for (var file in new Directory(dirScss).listSync())
       if ((file is File) && (!underscore.hasMatch(file.path)))
         Process.run('sass', [ '--scss', '--style=compressed', '--sourcemap=none',
-          file.path, compileDir+'/'+fileName.firstMatch(file.path).group(1)+'.css'])
+          file.path, dirCompile+'/'+fileName.firstMatch(file.path).group(1)+'.css'])
           ..then((proc){ if(proc.stderr.length>1) throw proc.stderr; });
     // Compile jade.
-    new File('jade.views.dart').writeAsStringSync(jade.renderDirectory(jadeDir));
-    views.loadLibrary();
+    new File('jade.views.dart').writeAsStringSync(jade.renderDirectory(dirJade));
   }
-  _handleRequest(HttpRequest req) {
-
+  _notFound(HttpResponse res) => res..statusCode = HttpStatus.NOT_FOUND..headers.contentType = ContentType.TEXT..write('File not found.')..close();
+  void _handleRequest(HttpRequest req) {
+    _log(req.method+": "+req.uri.path);
+    for(Route route in _routes)
+      if(req.method == route.method && req.uri.path == route.path && route.handler(req)) return;
+    if(req.method == 'GET') {
+      _log("Attempting static at "+dirPublic+req.uri.path);
+      File file = new File(dirPublic+req.uri.path);
+      if(file.existsSync()) {
+        req.response
+        ..statusCode = HttpStatus.OK
+        ..headers.contentType = _parseType(file.path)
+        ..write(file.readAsStringSync())
+        ..close();
+      }
+      else render(req.response, '404');
+    }
   }
+  ContentType _parseType(String file) {
+    // ifs
+    if(file.endsWith('.js')) return ContentType.parse('application/javascript; charset=utf-8');
+    for(String key in _types.keys)
+      if(_types.containsKey(key))
+        if(_types[key].hasMatch(file))
+          return ContentType.parse(key+'/'+_types[key].firstMatch(file).group(1)+';'+((key != 'image')?' charset=utf-8':''));
+    return ContentType.parse('text/plain; charset=utf-8');
+  }
+  void _log(String message) => print(message);
 }
+
+typedef bool RouteHandler(HttpRequest req);
+class Route { String method; String path; RouteHandler handler; Route(this.method,this.path,this.handler); }
